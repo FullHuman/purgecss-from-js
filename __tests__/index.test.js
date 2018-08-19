6a7dd315c51ce8bc839ad41b5dfad0059caee37a
Alina Ishchenko <alinaish@gmail.com>
2018-08-19 17:23:50 +0100

    add gitignore

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,4 +1,12 @@
+import fs from 'fs';
+
 describe('PurgeFromJS', () => {
+    beforeAll(() => {
+        fs.readFile(`${__dirname__}/mock-content.js`, (err, content)=> {
+            
+        });
+    });
+
     it('test', () => {
         const s = 1+3;
         expect(s).toBe(4);
---
+++ __tests__/mock-content.js
@@ -0,0 +1,3 @@
+const list = document.getElementsByTagName("p"); // get all p elements
+list.length; // show number of items
+list[0].style.color = "red";
