a40c4545b03346535d0b8175ca6286028fc95a41
Alina Ishchenko <alinaish@gmail.com>
2018-08-22 18:10:40 +0100

    fix tests

--- index.js
+++ index.js
@@ -1,31 +1,41 @@
 import * as esprima from 'esprima';
-import fs, { access } from 'fs';
-import path from 'path';
 
 export default class PurgeFromJS {
     static extract(content) {
         const tokens = esprima.tokenize(content);
-        let selectors = tokens.filter((token) => {
-            return token.type === 'Identifier' || token.type === 'Template' || token.type === 'String';
-        }).reduce((acc, token) => {
-            if (token.type === 'String') {
-                // cut single/double quotes from the string 
-                // because esprima wraps string to a string
-                const unwrappedString = token.value.slice(1, token.value.length - 1);
-                return acc.concat(unwrappedString.split(' ')); // in case if string contains a list of classes
-            } else if (token.type === 'Template') {
-                // cut backticks from the template
-                const len = token.value.length;
-                const isOpenedTemplate = token.value[0] === '`';
-                const isClosedTemplate = token.value[len - 1] === '`';
-                const unwrappedTemplate = token.value.slice(isOpenedTemplate ? 1 : 0, isClosedTemplate ? len - 1 : len);
-                return acc.concat(unwrappedTemplate.split(' '));
-            }
-            return acc.concat(token.value);
-        }, []);
-    
+        let selectors = tokens
+            .filter(token => {
+                return (
+                    token.type === 'Identifier' ||
+                    token.type === 'Template' ||
+                    token.type === 'String'
+                );
+            })
+            .reduce((acc, token) => {
+                if (token.type === 'String') {
+                    // cut single/double quotes from the string
+                    // because esprima wraps string to a string
+                    const unwrappedString = token.value.slice(
+                        1,
+                        token.value.length - 1
+                    );
+                    return acc.concat(unwrappedString.split(' ')); // in case if string contains a list of classes
+                } else if (token.type === 'Template') {
+                    // cut backticks from the template
+                    const len = token.value.length;
+                    const isOpenedTemplate = token.value[0] === '`';
+                    const isClosedTemplate = token.value[len - 1] === '`';
+                    const unwrappedTemplate = token.value.slice(
+                        isOpenedTemplate ? 1 : 0,
+                        isClosedTemplate ? len - 1 : len
+                    );
+                    return acc.concat(unwrappedTemplate.split(' '));
+                }
+                return acc.concat(token.value);
+            }, []);
+
         // clear selectors from empty strings
-        selectors = selectors.filter((selector) => selector !== '');
+        selectors = selectors.filter(selector => selector !== '');
         return [...new Set(selectors)]; // remove duplicates
     }
 }
--- index.test.js
+++ index.test.js
@@ -1,15 +1,26 @@
-import fs from 'fs';
 import PurgeFromJS from './index';
 
 describe('PurgeFromJS', () => {
-    const mockContent = `const list = document.getElementsByTagName('p\'kk\'');
+    const mockContent = `const list = document.getElementsByTagName('p'kk'');
     list.length;
-    list[0].style.color = \`red\$\{"list"\} \`;
+    list[0].style.color = \`red ${'list'} \`;
     const a = 'b';`;
 
     it('contains all the selectors', () => {
         const selectors = PurgeFromJS.extract(mockContent);
-        const expected = ['list', 'document', 'getElementsByTagName', 'p', 'kk', 'length', 'style', 'color', 'red${', '}', 'a', 'b'];
+        const expected = [
+            'list',
+            'document',
+            'getElementsByTagName',
+            'p',
+            'kk',
+            'length',
+            'style',
+            'color',
+            'red',
+            'a',
+            'b',
+        ];
         expect(selectors).toEqual(expected);
     });
 });
